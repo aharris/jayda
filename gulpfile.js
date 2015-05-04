32e9e0f20ae5f5617dd995b69cceb9168697c9d6
Ashton Harris <ashtonharris.me@gmail.com>
2015-05-03 22:53:23 -0400

    Add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -43,9 +43,62 @@ gulp.task('jade', function() {
     .pipe(connect.reload());
 });
 
+// gulp.task('js', function () {
+//   gulp.src('app/js/**/*.js')
+//     .pipe(gulp.dest('dest/js'));
+// });
+
 gulp.task('js', function () {
-  gulp.src('app/js/**/*.js')
-    .pipe(gulp.dest('dest/js'));
+  var browserify = require('browserify');
+  var source = require('vinyl-source-stream');
+  var buffer = require('vinyl-buffer');
+  var globby = require('globby');
+  var through = require('through2');
+  var uglify = require('gulp-uglify');
+  var sourcemaps = require('gulp-sourcemaps');
+  var reactify = require('reactify');
+
+  // gulp expects tasks to return a stream, so we create one here.
+  var bundledStream = through();
+
+  bundledStream
+    // turns the output bundle stream into a stream containing
+    // the normal attributes gulp plugins expect.
+    .pipe(source('app.js'))
+    // the rest of the gulp task, as you would normally write it.
+    // here we're copying from the Browserify + Uglify2 recipe.
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+      // Add gulp plugins to the pipeline here.
+      .pipe(uglify())
+      .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dest/js/'))
+    .pipe(connect.reload());
+
+  // "globby" replaces the normal "gulp.src" as Browserify
+  // creates it's own readable stream.
+  globby(['./app/js/**/*.js', './app/components/**/*.js'], function(err, entries) {
+    // ensure any errors from globby are handled
+    if (err) {
+      bundledStream.emit('error', err);
+      return;
+    }
+
+    // create the Browserify instance.
+    var b = browserify({
+      entries: entries,
+      debug: true,
+      transform: [reactify]
+    });
+
+    // pipe the Browserify stream into the stream we created earlier
+    // this starts our gulp pipeline.
+    b.bundle().pipe(bundledStream);
+  });
+
+  // finally, we return the stream, so gulp knows when this task is done.
+  return bundledStream;
 });
 
 gulp.task('tree', function () {
@@ -125,7 +178,7 @@ gulp.task('watch', function () {
   gulp.watch(['app/styl/**/*.styl', 'app/components/**/*.styl'], ['stylus']);
   gulp.watch(['./app/**/*.jade'], ['jade']);
   gulp.watch(['app/components/**/*.jade'], ['tree', 'templatizer']);
-  gulp.watch(['app/js/*.js'], ['js']);
+  gulp.watch(['app/js/**/*.js', 'app/components/**/*.js'], ['js']);
 
   // Jayda
   gulp.watch(['jayda/**/*'], ['jayda-jade']);

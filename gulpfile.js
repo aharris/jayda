a62b02a0e772fb23c2a6a48d9406d7b700aba121
Ashton Harris <ashtonharris.me@gmail.com>
2015-07-03 12:21:15 -0400

    Split gulpfile into an app and jayda specific one

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,9 @@ var gulp = require('gulp'),
   shell = require('gulp-shell'),
   gutil = require('gulp-util'),
   directoryMap = require("gulp-directory-map"),
-  data = require('gulp-data'),
   templatizer = require('templatizer'),
   karma = require('karma').server,
-  p = require('./package.json');
-
+  p = require('./jayda/package.json');
 
 
 // --------------------------------------
@@ -32,7 +30,7 @@ gulp.task('stylus', function () {
 
 gulp.task('stylint', function() {
   var stylint = require('gulp-stylint');
-  return gulp.src([p.config.appSrc + '/**/*.styl', './jayda/**/*.styl', '!./jayda/styl/lib**/*.styl'])
+  return gulp.src([p.config.appSrc + '/**/*.styl', './jayda/**/*.styl', '!./jayda/styl/lib**/*.styl', '!./jayda/node_modules/**/*.styl'])
     .pipe(stylint({config: '.stylintrc'}));
 });
 
@@ -103,11 +101,6 @@ gulp.task('js', function () {
   return bundledStream;
 });
 
-gulp.task('fonts', function () {
-  return gulp.src(p.config.appSrc + '/fonts/**/*')
-    .pipe(gulp.dest('dest/fonts'));
-});
-
 gulp.task('tree', function () {
   return gulp.src([p.config.appSrc + '/components/**/*.jade', p.config.appSrc + '/components/**/*.html', p.config.appSrc + '/components/**/*.js', p.config.appSrc + '/components/**/*.json'])
     .pipe(directoryMap({
@@ -138,113 +131,6 @@ gulp.task('libs', function() {
   gulp.src('node_modules/materialize-css/bin/materialize.js')
     .pipe(gulp.dest('dest/js/lib'));
 });
-
-// --------------------------------------
-// JAYDA --------------------------------
-// --------------------------------------
-
-gulp.task('jayda-jade', function () {
-  gulp.src(['./jayda/templates/**/*.jade', '!./jayda/templates/**/_*.jade'])
-    .pipe(jadeGlobbing())
-    .pipe(jade())
-    .on('error', gutil.log)
-    .pipe(gulp.dest('./dest/jayda'))
-    .pipe(connect.reload());
-});
-
-gulp.task('jayda-stylus', function () {
-  gulp.src(['./jayda/styl/**/*.styl', !p.config.appSrc + '/styl/**/_*'])
-    .pipe(stylus({use: [nib(), jeet(), rupture()]}))
-    .pipe(gulp.dest('./dest/jayda/css'))
-    .pipe(connect.reload());
-});
-
-gulp.task('jayda-js', function () {
-  var browserify = require('browserify');
-  var source = require('vinyl-source-stream');
-  var buffer = require('vinyl-buffer');
-  var globby = require('globby');
-  var through = require('through2');
-  var uglify = require('gulp-uglify');
-  var sourcemaps = require('gulp-sourcemaps');
-  var reactify = require('reactify');
-
-  // gulp expects tasks to return a stream, so we create one here.
-  var bundledStream = through();
-
-  bundledStream
-    // turns the output bundle stream into a stream containing
-    // the normal attributes gulp plugins expect.
-    .pipe(source('jayda.js'))
-    // the rest of the gulp task, as you would normally write it.
-    // here we're copying from the Browserify + Uglify2 recipe.
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-      // Add gulp plugins to the pipeline here.
-    .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dest/jayda/js/'))
-    .pipe(connect.reload());
-
-  // "globby" replaces the normal "gulp.src" as Browserify
-  // creates it's own readable stream.
-  globby([
-    './jayda/js/jayda.js',
-    './jayda/js/utils.js',
-    './jayda/js/router.js',
-    './jayda/js/jayda_jade.js',
-    './jayda/js/jayda_html.js',
-    './jayda/js/jayda_icons.js'
-  ], function(err, entries) {
-    // ensure any errors from globby are handled
-    if (err) {
-      bundledStream.emit('error', err);
-      return;
-    }
-
-    // create the Browserify instance.
-    var b = browserify({
-      entries: entries,
-      debug: true,
-      transform: [reactify]
-    });
-
-    // pipe the Browserify stream into the stream we created earlier
-    // this starts our gulp pipeline.
-    b.bundle().pipe(bundledStream);
-  });
-
-  // finally, we return the stream, so gulp knows when this task is done.
-  return bundledStream;
-});
-
-
-gulp.task('jayda-get-components', function () {
-  gulp.src([p.config.appSrc + '/components/**/*.js', p.config.appSrc + '/components/**/*.json', p.config.appSrc + '/components/**/*.html'])
-    .pipe(gulp.dest('dest/components'));
-
-  gulp.src('jayda/js/lib/*.js')
-    .pipe(gulp.dest('dest/jayda/js/lib'));
-});
-
-gulp.task('jayda-templatizer', function() {
-    templatizer('./jayda/components/**/*.jade', './dest/jayda/js/compiled_patterns.js', {
-      namespace: 'window.J.Jayda',
-      dontremoveMixins: true
-    });
-});
-
-gulp.task('jayda-images', function() {
-  gulp.src('jayda/images/**/*')
-    .pipe(gulp.dest('dest/images'));
-});
-
-gulp.task('jayda-font-icons', function() {
-  return gulp.src(p.config.iconFontFile)
-    .pipe(gulp.dest('dest/jayda/data'))
-    .pipe(connect.reload());
-});
-
 
 // --------------------------------------
 // Tests --------------------------------
@@ -281,51 +167,49 @@ gulp.task('connect', function() {
 
 gulp.task('watch', function () {
   gulp.watch([p.config.appSrc + '/styl/**/*.styl', p.config.appSrc + '/components/**/*.styl'], ['stylus', 'stylint']);
-  gulp.watch([p.config.appSrc + '/fonts/**/*'], ['fonts']);
   gulp.watch([p.config.appSrc + '/**/*.jade'], ['jade']);
   gulp.watch([p.config.appSrc + '/components/**/*.jade'], ['tree', 'templatizer']);
   gulp.watch([p.config.appSrc + '/js/**/*.js', p.config.appSrc + '/components/**/*.js', p.config.appSrc + '/components/**/*.html'], ['js', 'jayda-get-components']);
 
-  // Jayda
-  gulp.watch(['jayda/**/*.jade'], ['jayda-jade', 'jayda-templatizer']);
-  gulp.watch(['jayda/js/**/*.js'], ['jayda-js']);
-  gulp.watch(['jayda/styl/**/*.styl'], ['jayda-stylus', 'stylint']);
-  gulp.watch(['jayda/images/**/*'], ['jayda-images']);
-  gulp.watch(['jayda/data/icons.json'], ['jayda-font-icons']);
+  // JAYDA
+  gulp.watch(['./jayda/**/*.jade'], ['jayda-jade', 'jayda-templatizer']);
+  gulp.watch(['./jayda/js/**/*.js'], ['jayda-js']);
+  gulp.watch(['./jayda/styl/**/*.styl'], ['jayda-stylus', 'stylint']);
+  gulp.watch(['./jayda/images/**/*'], ['jayda-images']);
+  gulp.watch(['./jayda/data/icons.json'], ['jayda-font-icons']);
 });
+
+// Set Up Jayda
+gulp.task('jayda', shell.task([
+  'cd jayda && gulp'
+]));
 
 // --------------------------------------
 // Commands -----------------------------
 // --------------------------------------
-
 gulp.task('default', function(callback){
   runSequence(
     [
-    'clean'
+      'clean'
     ],
     [
       'mkdirs'
     ],
     [
       'libs',
-      'js',
-      'jayda-js',
-      'jayda-get-components',
-      'fonts',
-      'jayda-images'
+      'js'
     ],
     [
-    'tree'
+      'tree'
     ],
     [
       'stylus',
-      'jayda-stylus',
-      'jayda-font-icons',
       'stylint',
       'jade',
-      'jayda-jade',
       'templatizer',
-      'jayda-templatizer'
+    ],
+    [
+      'jayda'
     ],
     [
       'connect',

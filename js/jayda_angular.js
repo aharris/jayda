6e97808f72301842e1cbc33d09d2eca784616fd9
Ashton Harris <ashtonharris.me@gmail.com>
2015-07-25 23:55:21 -0400

    be deffensive about checking for angular specific elements

--- js/jayda_angular.js
+++ js/jayda_angular.js
@@ -3,7 +3,9 @@ J.bootstrapAngularApp =function () {
     console.error('You have defined that you are running an angular application but you have not defined a module name. Please add your module name to your package.json config.angularModuleName.');
     return;
   }
-  angular.element('#angular-example').ready(function() {
-    angular.bootstrap('#angular-example', [J.config.angularModuleName]);
-  });
+  if ($('#angular-example').length > 0) {
+    angular.element('#angular-example').ready(function() {
+      angular.bootstrap('#angular-example', [J.config.angularModuleName]);
+    });
+  }
 };
